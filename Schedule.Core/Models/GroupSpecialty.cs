64154631957511e8c1f641159ed01811d63dcdb5
Serhii Babenkov (HomePC) <admin@chelmash.com.ua>
2026-07-09 20:53:47 +0300

    Додав в Models class GroupSpecialty

---
+++ Schedule.Core/Models/GroupSpecialty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.Core.Models;
+
+public class GroupSpecialty
+{
+    public int Id { get; set; }
+
+    public int GroupId { get; set; }
+
+    public int SpecialtyId { get; set; }
+
+    public Group? Group { get; set; }
+
+    public Specialty? Specialty { get; set; }
+}
